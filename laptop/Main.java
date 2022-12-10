7256b2d6ec2962c707899b8b5d6175dbab7fc522
JudgeMe-2 <120168235+JudgeMe-2@users.noreply.github.com>
2022-12-10 16:17:14 +0800

    Update Main.java

--- laptop/Main.java
+++ laptop/Main.java
@@ -2,7 +2,16 @@ package com.laptop;
 
 public class Main {
     public static void main(String[] args) {
-        Laptop laptop = new LaptopBuilder().Ram(8).Brand("Dell").build();
-        System.out.println(laptop);
+        //Example 1
+        Laptop lap1 = new LaptopBuilder().Brand("Dell").Ram(8).build();
+        System.out.println(lap1);
+
+        //Example 2
+        Laptop lap2 = new LaptopBuilder().Brand("Lenovo").Ram(8).Processor("Intel").ScreenSize(15.6).BatteryCapacity(4000).build();
+        System.out.println(lap2);
+
+        //Example 3
+        Laptop lap3 = new LaptopBuilder().BatteryCapacity(4000).Brand("Acer").build();
+        System.out.println(lap3);
     }
 }
